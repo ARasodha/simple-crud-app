e112fc2ff9b43bb5f96ea981a32c870e2f1544c9
Arjun Rasodha <arjunrasodha@Arjuns-MacBook-Pro.local>
2022-05-30 21:00:55 -0400

    mongodb routes fixed

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const {Pool} = require('pg');
 const mongoose = require('mongoose');
 require('dotenv').config();
-
+const Mongoosedata = require('./models/mongodata');
 mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true })
 
 const pool = new Pool();
@@ -17,11 +17,7 @@ const PORT = process.env.PORT;
 
 app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.json()) // not sure if needed with bodyParser
-
-const mongodataRouter = require('./routes/mongodata')
-app.use('/mongodata', mongodataRouter)
-
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
@@ -74,6 +70,71 @@ app.post('/info/update', (req, res) => {
         console.log(error);
     }
 })
+
+app.get('/mongodata', async (req, res) => {
+  try {
+    const data = await Mongoosedata.find();
+    res.send(data);
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
+  
+})
+
+app.post('/mongodata', getMongoosedata, async (req, res) => {
+  const mongoosedata = new Mongoosedata({
+    name: req.body.name,
+    age: req.body.age
+  })
+
+  try {
+    const newMongooseData = await mongoosedata.save();
+    res.status(201).send(newMongooseData);
+  } catch (error) {
+    res.status(400).json({ message: error.message })
+  }
+})
+
+app.patch('/mongodata/:id', getMongoosedata, async (req, res) => {
+  if (req.body.name != null) {
+    res.mongoosedata.name = req.body.name
+  }
+
+  if (req.body.age != null) {
+    res.mongoosedata.age = req.body.age
+  }
+
+  try {
+    const updatedMongoosedata = await res.mongoosedata.save()
+    res.json(updatedMongoosedata)
+  } catch (error) {
+    res.status(400).json({ message: error.message })
+  }
+})
+
+app.delete('/mongodata/:id', getMongoosedata, async (req, res) => {
+  try {
+    await res.mongoosedata.remove()
+    res.json({ message: 'Mongoosedata deleted' })
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
+})
+
+async function getMongoosedata(req, res, next) {
+  let mongoosedata;
+  try {
+      mongoosedata = await Mongoosedata.findById(req.params.id)
+      if (mongoosedata == null) {
+          return res.status(404).json({ message: 'Cannot find mongoosedata with specified id'});
+      }
+  } catch (error) {
+    return res.status(500).json({ message: error.message })
+  }
+
+  res.mongoosedata = mongoosedata;
+  next()
+}
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
